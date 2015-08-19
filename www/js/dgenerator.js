cd46df5268b6ddcb6013bad2422f6d93d6dcd7eb
phact <estevezsebastian@gmail.com>
2015-08-19 14:39:50 -0400

    Issue #3

--- www/js/dgenerator.js
+++ www/js/dgenerator.js
@@ -6,16 +6,44 @@ $(document).ready(function() {
 	success: function(data) {processData(data);}
 	});
 	
+	$("#opsc_ip").textinput("disable")	
 	$("#node_topology").textinput("disable")	
+	$("#cluster_config").textinput("disable")	
 	$("#node_label").css("color","lightgray")
+	$("#opsc_ip_label").css("color","lightgray")
+	$("#config_label").css("color","lightgray")
+
+
+	$('#opsc_ip').on('change', function(){
+
+		$('#opsc_url')[0].href= "http://"+$("#opsc_ip").val() +":8888/cluster-configs"
+		$('#opsc_url').text("here")
+
+	})
+
+  $('#cluster_config').on('change', function(){
+
+		$('#opsc_url_2')[0].href= "http://"+$("#opsc_ip").val() +":8888/"+Object.keys(JSON.parse($("#cluster_config").val()))[0]+"/nodes"
+		$('#opsc_url_2').text("here")
+
+	})
+
 
 	$('#top-radio').on('change', function(){      
 		if ( $("#top-radio :checked")[0].value== "Each Node"){
+			$("#opsc_ip").textinput("enable")	
 			$("#node_topology").textinput("enable")
 			$("#node_label").css("color","black")
+			$("#opsc_ip_label").css("color","black")
+			$("#config_label").css("color","black")
+			$("#cluster_config").textinput("enable")	
 		}else {
+			$("#opsc_ip").textinput("disable")	
 			$("#node_topology").textinput("disable")
 			$("#node_label").css("color","lightgray")
+			$("#opsc_ip_label").css("color","lightgray")
+			$("#config_label").css("color","lightgray")
+			$("#cluster_config").textinput("disable")	
 		 }
 	});    
 
